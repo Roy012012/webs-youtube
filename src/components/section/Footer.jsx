57d0ece6afd486cd6c04ef63b75d6a5a90ad5423
Roy012012 <goodroy@kakao.com>
2024-02-13 16:51:33 +0900

    추천 영상 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer id='footer' role="contentinfo"> 
-            footer
+            <a href="mailto:yun4631@naver.com" rel="noopener noreferrer">
+                yun4631@naver.com
+            </a>
         </footer>
     )
 }
